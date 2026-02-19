6743e4ba1c885e2f4386aacefb57af37b91580be
josemlrod <mrjosemlrod@gmail.com>
2026-02-18 23:45:30 -0500

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,21 @@ import { Suspense } from 'react';
 import { HeroCarousel } from '@/components/hero-carousel';
 import { ProductGrid } from '@/components/product-grid';
 
-const DOMAIN =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://volt-mocha.vercel.app';
-
 export default async function Home() {
   const getFeaturedProductsResponse = await fetch(
-    `${DOMAIN}/get-featured-products`,
+    `${process.env.NEXT_PUBLIC_API_URL}/get-featured-products`,
     {
       method: 'GET',
     },
   );
   const { data: featured } = await getFeaturedProductsResponse.json();
 
-  const allProductsResponse = await fetch(`${DOMAIN}/all-products`, {
-    method: 'GET',
-  });
+  const allProductsResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/all-products`,
+    {
+      method: 'GET',
+    },
+  );
   const { data: allProducts } = await allProductsResponse.json();
 
   return (
