640c687a0f10a8d4ec9cba5947a89349bbccc082
josemlrod <mrjosemlrod@gmail.com>
2026-02-18 23:53:43 -0500

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,22 @@ import { Suspense } from 'react';
 import { HeroCarousel } from '@/components/hero-carousel';
 import { ProductGrid } from '@/components/product-grid';
 
+const DOMAIN = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
+
 export default async function Home() {
   const getFeaturedProductsResponse = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/get-featured-products`,
+    `${DOMAIN}/get-featured-products`,
     {
       method: 'GET',
     },
   );
   const { data: featured } = await getFeaturedProductsResponse.json();
 
-  const allProductsResponse = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/all-products`,
-    {
-      method: 'GET',
-    },
-  );
+  const allProductsResponse = await fetch(`${DOMAIN}/all-products`, {
+    method: 'GET',
+  });
   const { data: allProducts } = await allProductsResponse.json();
 
   return (
