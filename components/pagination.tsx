74af3eb102787b607ac44cfeff7fa165fa86b299
Maximo Espinal <113195223+NojovenNo@users.noreply.github.com>
2026-02-23 23:12:41 -0400

    adding pagination ui (#20)

---
+++ components/pagination.tsx
@@ -0,0 +1,46 @@
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
+  import { categories, Product } from '@/lib/products';
+
+  type PaginationPageProps = {
+    currentPage: number;
+    totalPages: number;
+    basePath?: string;
+  };
+
+  export function PaginationPage({currentPage,totalPages,basePath}:PaginationPageProps) {
+    return (
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#" isActive>1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">2</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">10</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    )
+  }
