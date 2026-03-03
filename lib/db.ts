9614646ff1516a19e0a9a29608c93b6a0eab5d9a
josemlrod <mrjosemlrod@gmail.com>
2026-03-02 22:16:58 -0500

    fix build

--- lib/db.ts
+++ lib/db.ts
@@ -1,28 +1,22 @@
 'use server';
 import { neon, type NeonQueryFunction } from '@neondatabase/serverless';
 
-let db: NeonQueryFunction<false, false> | null = null;
+let initializedDb: NeonQueryFunction<false, false> | null = null;
 
 function initDb(): NeonQueryFunction<false, false> {
-  if (db) return db;
-  db = neon(process.env.STORAGE_DATABASE_URL as string);
-  return db;
+  if (initializedDb) return initializedDb;
+  initializedDb = neon(process.env.STORAGE_DATABASE_URL as string);
+  return initializedDb;
 }
 
 export async function getProducts() {
-  if (!db) {
-    initDb();
-  }
-
+  const db = initDb();
   const products = await db`SELECT * FROM products`;
   return products;
 }
 
 export async function getFeaturedProducts() {
-  if (!db) {
-    initDb();
-  }
-
+  const db = initDb();
   const featuredProducts =
     await db`SELECT * FROM products WHERE featured IS TRUE`;
   return featuredProducts;
